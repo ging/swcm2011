b2d55d903e3693abc4debd68f8479a769c849dee
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2011-03-04 10:27:40 +0100

    Count rdoc and tasks

--- WordCount/lib/Count.rb
+++ WordCount/lib/Count.rb
@@ -1,5 +1,13 @@
-module Count  # Incluye el método "count_words" en el modulo "Count"
+# Count incluye el método "count_words" para contar palabras
+module Count
 
+  # Cuenta palabras. Cada palabra debe estar formada solo por letras ASCII
+  #
+  # Se reconocen con el patrón [a-zA-Z]+
+  #
+  # Se ha implementado con un patron que reconoce cada palabra y
+  # la sustituye por la letra w. Luego se calcula la longitud de la cadena
+  # generada
   def count_words(text)
     if (text =~ /[^a-zA-Z]*\\Z/) then text = "" end  # elimina ficheros sin palabras
 
